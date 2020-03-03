d63f07774551c01d219d67170563123534f9764b
unn04012 <unn04012@naver.com>
2020-03-03 10:49:13 +0900

    방문자 통계(1)

--- login_check.php
+++ login_check.php
@@ -1,6 +1,5 @@
 <?php
 include("../login/connect.php");
-
 function getBrowserInfo()
 {
   $userAgent = $_SERVER["HTTP_USER_AGENT"];
@@ -69,6 +68,105 @@ if(!($mb['mb_id']==$mb_id) || !($mb['mb_password']==$mb_password)){
   exit;
 }
 
+// 테이블 구조 : uid, ipaddr, date, time, OS, browser, userID, hit
+// SESSION 이 살아있는 동안에는 카운트 안되도록 처리
+$access_ip=$_SERVER['REMOTE_ADDR'];
+$getOS = getOS(); // 접속 OS 정보
+$getBrowser = getBrowser(); // 브라우저 접속 정보
+$date = date("Y-m-d", time()); // 오늘날짜
+$time = date("H:i:s", time()); // 시간
+$mb_id = $mb_id ? $mb_id : 'none';
+$sql ="SELECT count(*) FROM accesslog WHERE date ='$date' AND userID = '$mb_id  '";
+$result=mysqli_query($conn, $sql);
+$row = mysqli_fetch_row($result);
+
+  if($row[0]==0){ // 오늘 접속날짜 기록이 없으면
+    $sql2 = "INSERT INTO accesslog (ipaddr,date,time,OS,browser,userID,hit,log)
+              VALUES ('$access_ip','$date','$time','$getOS','$getBrowser','$mb_id','1','1')";
+    $result2=mysqli_query($conn, $sql2);
+
+  } else { // 접속 기록이 있으면 해당 IP주소의 카운트만 증가시켜라.
+    $sql = "UPDATE accessLog SET hit=hit+1 Where ipaddr='$access_ip' AND userID = '$mb_id'";
+    $result=mysqli_query($conn, $sql);
+  }
+
+
+
+// 접속 Device
+function user_agent(){
+$iPod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$iPhone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$iPad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+if($iPad||$iPhone||$iPod){
+  return 'ios';
+} else if($android){
+  return 'android';
+} else {
+  return 'etc';
+}
+}
+
+function getOS() {
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+$os_platform    =   "Unknown OS Platform";
+$os_array       =   array(
+            '/windows nt 10/i'     =>  'Windows 10',
+            '/windows nt 6.3/i'     =>  'Windows 8.1',
+            '/windows nt 6.2/i'     =>  'Windows 8',
+            '/windows nt 6.1/i'     =>  'Windows 7',
+            '/windows nt 6.0/i'     =>  'Windows Vista',
+            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+            '/windows nt 5.1/i'     =>  'Windows XP',
+            '/windows xp/i'         =>  'Windows XP',
+            '/windows nt 5.0/i'     =>  'Windows 2000',
+            '/windows me/i'         =>  'Windows ME',
+            '/win98/i'              =>  'Windows 98',
+            '/win95/i'              =>  'Windows 95',
+            '/win16/i'              =>  'Windows 3.11',
+            '/macintosh|mac os x/i' =>  'Mac OS X',
+            '/mac_powerpc/i'        =>  'Mac OS 9',
+            '/linux/i'              =>  'Linux',
+            '/ubuntu/i'             =>  'Ubuntu',
+            '/iphone/i'             =>  'iPhone',
+            '/ipod/i'               =>  'iPod',
+            '/ipad/i'               =>  'iPad',
+            '/android/i'            =>  'Android',
+            '/blackberry/i'         =>  'BlackBerry',
+            '/webos/i'              =>  'Mobile'
+          );
+foreach ($os_array as $regex => $value) {
+  if (preg_match($regex, $user_agent)) {
+    $os_platform    =   $value;
+  }
+}
+return $os_platform;
+}
+
+function getBrowser() {
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+$browser        =   "Unknown Browser";
+$browser_array  =   array(
+            '/msie/i'       =>  'Internet Explorer',
+            '/firefox/i'    =>  'Firefox',
+            '/safari/i'     =>  'Safari',
+            '/chrome/i'     =>  'Chrome',
+            '/edge/i'       =>  'Edge',
+            '/opera/i'      =>  'Opera',
+            '/netscape/i'   =>  'Netscape',
+            '/maxthon/i'    =>  'Maxthon',
+            '/konqueror/i'  =>  'Konqueror',
+            '/mobile/i'     =>  'Mobile Browser'
+          );
+foreach ($browser_array as $regex => $value) {
+  if (preg_match($regex, $user_agent)) {
+    $browser    =   $value;
+  }
+}
+return $browser;
+}
+
+//end class
 if($result && ($mb['mb_password'] == $mb_password)){
   if($mb_id ==="admin"){
     $_SESSION['ss_mb_id'] = $mb_id;
@@ -108,11 +206,12 @@ if($result && ($mb['mb_password'] == $mb_password)){
     echo "<script> alert('차단 된 아이디입니다'); </script>";
     echo "<script> location.replace('./post.php'); </script>";
     exit;
-  }  
+  }
   echo "<script> alert('로그인 성공'); </script>";
   echo "<script> location.replace('./post.php'); </script>";
   exit;
 }
+
 
 
 mysqli_close($conn);
