636dbacade617de8cbf75382083ba3dd5fbfaa54
= <=>
2020-03-31 01:32:07 +0900

    리팩토링(함수화)

--- homepage.php
+++ homepage.php
@@ -1,57 +1,91 @@
 <?php
-include("../login/connect.php");
-include('./multi_login.php');
-include('./auto_logout.php');
- ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
-</head>
-    <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./homepage.css">
-  </head>
-  <style media="screen">
-  </style>
-  <body>
-    <?php if($_SESSION['ss_mb_id']) {?>
-      <div class="top">
-        <?php echo $_SESSION['ss_mb_id'] ?>님 환영합니다
-        <a href="./logout.php">로그아웃</a>
-      </div>
-  <?php }else{?>
-    <div class="top">
-      <a href="./login.php">로그인</a>
-      <a href="../login/register.php">회원가입</a>
-    </div>
-  <?php } ?>
-    <div class="header">
-      <h2 class = "logo"><a href="./homepage.php">Homepage</a></h2>
-      <input type="checkbox" id ="chk" value="">
-      <label for="chk" class="show-menu-btn">
-        <i class="fas fa-ellipsis-h"></i>
-      </label>
+class Page{
+  // page 속성
+  public $content;
+  public $title = "Homepage";
+  public $image;  
+ // page 클래스의 메서드(함수)
+ public function __set($name, $value){
+   $this->$name = $value;
+ }
 
-      <ul class="menu">
-         <a href="./homepage.php">Home</a>
-         <a href="./post.php">community</a>
-         <a href="./user_board.php">Services</a>
-         <a href="./visitor_statistic.php">Works</a>
-        <a href="#">Contact</a>
-        <label for="chk" class="hide-menu-btn">
-          <i class="fas fa-times"></i>
-        </label>
-      </ul>
+ public function Display(){
+   echo "<html>\n<head>\n";
+   $this->DisplayTitle();
+   $this->DisplayKeywords();
+   $this->DisplayStyles();
+   $this->DisplayHeader();
+   echo "</head>\n<body>\n";
+   $this->DisplayContent();
+   $this->DisplayFooter();
+   echo "</body>\n</html>\n";
+ }
+
+ public function DisplayTitle(){
+   echo "<title> $this->title </title>";
+ }
+
+ public function DisplayKeywords(){
+   ?>
+   <meta charset = "utf8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <?php
+ }
+
+ public function DisplayStyles(){
+   ?>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
+   <link rel="stylesheet" href="./homepage.css">
+   <?php
+ }
+
+ public function DisplayHeader(){
+   ?>
+   <!-- page header -->
+   <div class="header">
+     <h2 class = "logo"><a href="./homepage.php">Homepage</a></h2>
+     <input type="checkbox" id ="chk" value="">
+     <label for="chk" class="show-menu-btn">
+       <i class="fas fa-ellipsis-h"></i>
+     </label>
+     <ul class="menu">
+        <a href="./homepage.php">Home</a>
+        <a href="./post.php">community</a>
+        <a href="./user_board.php">Services</a>
+        <a href="./visitor_statistic.php">Works</a>
+       <a href="#">Contact</a>
+       <label for="chk" class="hide-menu-btn">
+         <i class="fas fa-times"></i>
+       </label>
+     </ul>
     </div>
-    <div class="content">
-      <center>
-        <img src="background.jpg" alt="">
-      </center>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quo, dignissimos harum error assumenda, facere nam, veniam natus minus dolorum earum adipisci dolorem perspiciatis maiores fuga deserunt qui. Harum porro impedit illum minus dolor veniam quibusdam blanditiis labore consectetur ut suscipit quis quaerat libero provident sed incidunt qui molestiae, saepe nobis quisquam facilis commodi! Voluptate hic mollitia voluptatem, repudiandae voluptatum cum nisi. Aliquid officia hic animi, qui, recusandae consequatur obcaecati perferendis quisquam earum deleniti quis tenetur incidunt sit sunt, cumque optio eius. Debitis facere ratione aperiam provident, perspiciatis beatae deleniti, ea voluptatem modi quia sint veritatis magnam accusamus deserunt facilis dolor ipsum similique incidunt praesentium. Porro officiis a voluptate reprehenderit dignissimos beatae dolores. Omnis nam consectetur nobis eos quis, natus, odit consequatur possimus est asperiores fuga! In, ipsam laudantium blanditiis, ratione illo voluptate aspernatur quisquam dicta. Inventore iusto, modi repellendus debitis corporis labore, laudantium fugiat porro sunt, ullam distinctio minus odio, rerum expedita quis placeat illo nostrum facilis numquam? Nisi eligendi, pariatur sint commodi explicabo, debitis voluptate delectus aliquid quisquam! Aut quaerat et expedita voluptas sit inventore excepturi, cumque maxime suscipit, possimus magnam nulla voluptatem quam tenetur, nesciunt nam! Magnam laudantium accusantium quidem ut facere reiciendis dolor quaerat iure fugiat, voluptatem officia, est et, eligendi necessitatibus velit nesciunt quibusdam nulla cum? Aperiam id dolores architecto aliquam, cum officia aut alias sapiente commodi velit consectetur inventore, eum nisi natus molestiae odit amet quaerat perferendis a tempore eius, eos explicabo consequuntur repellat adipisci. Placeat facilis quaerat aliquid aspernatur doloribus odio nemo non vitae quidem provident fuga, consequatur, magni nam, dolores sit, repellendus? Reprehenderit dignissimos unde ex aspernatur, quia delectus, voluptatum ipsa odit sint, neque dolor vero eaque nam dolorem non. Enim veniam odit iure error est ut deserunt omnis laudantium repudiandae. Commodi voluptate consectetur deserunt iste eius ab asperiores laudantium magni consequuntur libero, possimus dolorum, inventore quas nam aperiam. Aliquid tempore fuga laboriosam. Accusamus in nisi dolorum blanditiis impedit iusto reiciendis veritatis cumque voluptate architecto, a consequuntur nihil ex mollitia doloribus ipsum consequatur nulla repellat! Doloribus porro asperiores assumenda nostrum officia sed quod non odit iusto culpa hic omnis, aliquid quae sequi ipsum. Maiores fuga laudantium saepe odit dolorum deleniti at labore nostrum minus dolore, laborum dolorem praesentium autem recusandae incidunt laboriosam reprehenderit quibusdam possimus cumque quos perferendis reiciendis ab voluptates accusantium. Voluptas nemo provident odit ut facilis explicabo asperiores, ad alias numquam consectetur enim saepe suscipit ipsam quam officia modi maiores. Voluptatem obcaecati, asperiores. Incidunt facilis voluptatum expedita ipsum provident accusantium rem molestiae excepturi nemo impedit numquam dolorem reprehenderit cumque quam maxime a reiciendis, beatae illum hic aliquid voluptates. Magnam laudantium, aperiam nulla aut earum animi, optio fugiat dolorem consequatur, reiciendis tempora. Architecto cumque possimus quia autem, ex facilis non consequuntur ducimus est nihil earum quos velit totam libero cupiditate nisi facere, expedita officiis minus vitae, dolor ut dolorem. Animi obcaecati sequi, ut, atque eaque ratione aliquid voluptate ducimus quam fuga quis dolor sit. Ipsa rem, reprehenderit vero autem unde. Aperiam harum cupiditate enim, adipisci nobis praesentium perferendis. Ipsa quisquam, eaque.
-      </p>
-    </div>
-  </body>
-</html>
+   <?php
+ }
+
+ public function DisplayContent(){
+   ?>
+   <div class="content">
+     <center>
+       <?php echo $this->image; ?>
+     </center>
+     <p>
+       <?php echo $this->content; ?>
+     </p>
+   </div>
+   <hr>
+   <?php
+ }
+ public function DisplayFooter(){
+   ?>
+   <!-- page footer -->
+   <footer>
+     <p>
+       &copy; homepage ltd.<br>
+       Please see our
+       <a href="#">legal information page</a>
+     </p>
+   </footer>
+   <?php
+ }
+}
+ ?>
